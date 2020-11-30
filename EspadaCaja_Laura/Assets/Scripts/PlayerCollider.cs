d6bdd306aae9a26e3acace98456fd346efc20eb5
LEspadaC <72516160+LEspadaC@users.noreply.github.com>
2020-11-30 19:11:57 +0100

    E.a Colisiones
    Cuando el jugador choca con los obstaculos, desaparecen

---
+++ EspadaCaja_Laura/Assets/Scripts/PlayerCollider.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollider : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Obstacle(Clone)")
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
