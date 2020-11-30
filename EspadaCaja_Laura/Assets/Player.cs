2d0d0887c351bf7e10bd826efaa80df36b355c55
LEspadaC <72516160+LEspadaC@users.noreply.github.com>
2020-11-30 16:15:24 +0100

    A.a Interactividad
    La esfera ya se mueve por el plano.

---
+++ EspadaCaja_Laura/Assets/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 2.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movePlayer();
+    }
+
+    void movePlayer()
+    {
+        float posX = transform.position.x;
+        float posZ = transform.position.z;
+
+        float desplZ = Input.GetAxis("Vertical");
+        float desplX = Input.GetAxis("Horizontal");
+
+        transform.Translate(Vector3.right * Time.deltaTime * speed * desplX);
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * desplZ);
+
+    }
+}
